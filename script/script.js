d2c55a21482842c534048a59921c56386bf4757a
Oscar Heredia <u201913874@upc.edu.pe>
2024-11-26 22:38:52 -0500

    actividad 2

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function showMenu (){
+    let content = document.getElementById("barraLateral");
+    content.style.display = 'block';
+}
+
+function closeMenu (){
+    let content = document.getElementById("barraLateral");
+    content.style.display = 'none';
+}
